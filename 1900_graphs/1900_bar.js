1667f3c0799d36c0a4eab696c9d0ba6141d38484
TheLegacy-Coder <klaudiofushakako@gmail.com>
2026-02-10 16:49:23 -0500

    Created all the files for the charts. Also adapted the original CSV to only include 1900 and 2020 temperature data.

---
+++ 1900_graphs/1900_bar.js
@@ -0,0 +1,18 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+let serverData = await fetch("../temperature_data_1900_and_2020.csv")
+    .then(response => response.text())
+    .then(dataString => {return dataString});
+
+let parsedData = d3.csvParse(serverData);
+
+for (let i = 0; i < parsedData.length; i++) {
+    parsedData[i].Value = parseFloat(parsedData[i].Value);
+};
+
+console.log(parsedData);
+
+
+document.onload = () => {
+    const barContainer1900 = d3.select("#1900_bar");
+}
